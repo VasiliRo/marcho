1929d67243dbaed33f2f8ac54dabf453aa9b8a9a
VasiliRo <wod_vasia@mail.ru>
2021-04-12 19:38:01 +0300

    added contacts page

--- app/js/main.js
+++ app/js/main.js
@@ -120,3 +120,12 @@ $(function(){
     
 
 });
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
